8625ec05cf95ee7d9e7250eeacd0bc9b71387198
koyoo--maxwel <koyoomaxwel@.com>
2018-09-17 10:45:20 +0300

    deployment

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export SECRET_KEY='pitchit'
-export MAIL_USERNAME='koyoomaxwel@gmail.com'
-export MAIL_PASSWORD='12091995malindi?'
+export MAIL_USERNAME=koyoomaxwel@gmail.com
+export MAIL_PASSWORD=12091995malindi?
 
 
 python3 manage.py server
