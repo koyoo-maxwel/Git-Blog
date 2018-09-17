c80aa2fca40065092da76b79c2f98703f35c4cea
koyoo--maxwel <koyoomaxwel@.com>
2018-09-17 15:11:07 +0300

    about to deploy

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ class Config:
 
 class ProdConfig(Config):
     #SQLALCHEMY_DATABASE_URI = os.environ.get("HEROKU_POSTGRESQL_BROWN_URL")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("HEROKU_POSTGRESQL_MAROON_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     pass
 
 class TestConfig(Config):
